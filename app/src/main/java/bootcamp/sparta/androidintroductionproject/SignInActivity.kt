5c6f285ce98660b9aa9e6950d9b8463806a45c5c
SeoJungHan <seojh5939@gmail.com>
2023-08-01 11:18:58 +0900

    [FEAT] : ⚙ SignInActivity 버튼 인텐트 && 기능구현완료

--- app/src/main/java/bootcamp/sparta/androidintroductionproject/SignInActivity.kt
+++ app/src/main/java/bootcamp/sparta/androidintroductionproject/SignInActivity.kt
@@ -1,11 +1,51 @@
 package bootcamp.sparta.androidintroductionproject
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var idEditText: EditText
+    lateinit var pwEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        idEditText = findViewById<EditText>(R.id.et_id)
+        pwEditText = findViewById<EditText>(R.id.et_pw)
     }
+
+    fun onClickedButton(view: View) {
+        when (view.id) {
+            R.id.btn_login -> {
+                if (inputIsEmpty()) {
+                    showToastMsg(this, getString(R.string.toast_et_Empty))
+                }
+
+                if (!inputIsEmpty()) {
+                    val userId = idEditText.text.toString()
+                    val intent = Intent(this, HomeActivity::class.java)
+                    intent.putExtra("idDatFromSignInActivity", userId)
+                    startActivity(intent)
+
+                    showToastMsg(this, getString(R.string.toast_login_success))
+                }
+            }
+
+            R.id.btn_join -> {
+                val intent = Intent(this, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    // editText의 값이 비어있는지 체크
+    fun inputIsEmpty(): Boolean = idEditText.text.isEmpty() || pwEditText.text.isEmpty()
+    fun showToastMsg(context: Context, msg: String) =
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+
 }
